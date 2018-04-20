774388bdea248ec00cc828dd6611347906a43ccb
Gabe Huinker <ghuinker@iastate.edu>
2018-04-20 10:27:04 -0500

    FileView and Editor Main.py

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ class Main(Frame):
             split = filename.split('.')
             if split[1] == 'xlsx':
                 self.excelfiles.append(split[0])
-
+        print(self.excelfiles)
         self.curfile = None
 
 
@@ -37,6 +37,7 @@ class Main(Frame):
         self.create = Button(self,text="Create", command=self.create)
         self.runbtn = Button(self,text="Run File", command=self.runexcel)
         self.openexcelbtn = Button(self, text='Open Excel', command=self.openexcel )
+        self.deletebtn = Button(self, text = 'Delete Excel', command=self.deletefile)
 
         self.lb = Listbox(self)
 
@@ -46,21 +47,24 @@ class Main(Frame):
         self.lb.bind('<<ListboxSelect>>', self.excelclicked)
 
         self.excelnamelbl.grid(row=0, column = 0)
-        self.excelname.grid(row=0, column=1, columnspan=2)
+        self.excelname.grid(row=0, column=1, columnspan=3)
         self.create.grid(row=1, column=0)
         self.openexcelbtn.grid(row=1, column=1)
         self.runbtn.grid(row=1, column=2)
+        self.deletebtn.grid(row=1, column=3)
 
 
-        self.lb.grid(row=2, column=0, columnspan=2)
+        self.lb.grid(row=2, column=0, columnspan=3)
 
     def create(self):
-        try:
-            xlrd.open_workbook(self.excelname.get("1.0", 'end-1c') + '.xlsx')
-
-        except:
-            print("creating New File")
-            workbook = xlsxwriter.Workbook(self.excelname.get("1.0", 'end-1c') + '.xlsx')
+        name = self.excelname.get().strip()
+        if name in self.excelfiles:
+            self.runexcel()
+        else:
+            print(name)
+            if name == '':
+                return
+            workbook = xlsxwriter.Workbook(name+ '.xlsx')
             setup = workbook.add_worksheet("Setup")
             attempts = workbook.add_worksheet("Attempts")
             scores = workbook.add_worksheet("Scores")
@@ -71,20 +75,32 @@ class Main(Frame):
             setup.write('B1', 'Last Name')
             setup.write('C1', 'Sex')
             setup.write('D1', 'Level')
-
+            
+            setup.write('F1', 'Heats')
             setup.write('G1', 'Route')
             setup.write('H1', 'Scores')
 
-
-
-            print("saved")
             workbook.close()
-            self.updatesetup()
-            self.quit()
+            self.openexcel()
+            self.updatefileviewer()
 
 
-    def updateexcel(self):
-        None
+    def updatefileviewer(self):
+        files = []
+        mypath = os.getcwd()
+        for (dirpath, dirnames, filenames) in walk(mypath):
+            files.extend(filenames)
+            break
+
+        self.excelfiles = []
+        for id, filename in enumerate(files):
+            split = filename.split('.')
+            if split[1] == 'xlsx':
+                self.excelfiles.append(split[0])
+
+        self.lb.delete(0, END)
+        for excelfile in self.excelfiles:
+            self.lb.insert(END, excelfile)
 
     def excelclicked(self, e):
         try:
@@ -94,6 +110,10 @@ class Main(Frame):
         self.excelname.delete(0,END)
         self.excelname.insert(0,self.excelfiles[self.curfile])
 
+    def deletefile(self):
+        os.remove(self.excelname.get() + '.xlsx')
+        self.updatefileviewer()
+
     def openexcel(self):
         file = self.excelname.get() + '.xlsx'
         os.startfile(file)
@@ -101,12 +121,6 @@ class Main(Frame):
     def runexcel(self):
         window = Toplevel(self)
         app = Dash(window, self.excelname.get() + '.xlsx')
-
-    def updatesetup(self):
-        window = Toplevel(self)
-        frame = Frame(window, width=100, height=50)
-        frame.place(x=700, y=0)
-        label = Label(frame, text="Go Update Setup").pack()
 
     def quit(self):
         self.master.destroy()
